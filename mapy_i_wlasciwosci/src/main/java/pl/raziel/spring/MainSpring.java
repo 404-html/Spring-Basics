c44ea3ac7538696fbdc5cb362f51127282bdfb54
mietekzul <noname_18@wp.pl>
2017-05-25 23:03:51 +0200

    Wstrzykiwanie map i właściwości.

---
+++ mapy_i_wlasciwosci/src/main/java/pl/raziel/spring/MainSpring.java
@@ -0,0 +1,19 @@
+package pl.raziel.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainSpring {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+
+		SomeBean someBean = (SomeBean) context.getBean("someBean");
+		SomeBean otherBean = (SomeBean) context.getBean("otherBean");
+
+		System.out.println(someBean.getLocales());
+		System.out.println(someBean.getSettings());
+
+		System.out.println("\n" + otherBean.getLocales());
+		System.out.println(otherBean.getSettings());
+	}
+}
